2c979cef012e0cda0c6a4b1817c9f0a439e0ca23
Yan_C <58877417+Yan-C20@users.noreply.github.com>
2023-04-18 16:10:58 +0800

    Add files via upload

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,42 @@
+//import { useSession, signOut } from 'next-auth/client';
+
+import Link from 'next/link';
+import classes from './main-navigation.module.css';
+import { signOut, useSession } from 'next-auth/react';
+
+function MainNavigation() {
+  const { data: session, status} = useSession();
+
+  function logoutHandler() {
+      signOut();
+    }
+
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+          <div className={classes.logo}>Next Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!session && status && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
